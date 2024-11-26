88cd52d98edfa36ef7f0595136f57b26762bdd82
harshit960 <raj.harshit960@gmail.com>
2024-11-26 23:32:01 +0530

    Restore logo image in NavBar and comment out waitlist link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,7 +13,7 @@ function NavBar() {
         <>
             <div className="bg-white lg:w-4/5 w-11/12 flex p-3 lg:p-4 rounded-full justify-between items-center z-10">
                 <div className="logo">
-                    {/* <img src="assets/Frame 4.png" alt="logo" className='w-32' /> */}
+                    <img src="assets/Frame 4.png" alt="logo" className='w-32' />
                 </div>
                 <div className="lg:flex hidden flex-grow justify-center">
                     {navItems.map((item, index) => (
@@ -23,7 +23,7 @@ function NavBar() {
                 </div>
                 <div className="flex justify-center items-center">
                     <a href="https://dh4ftmjzfzn.typeform.com/to/XuzeNABu" target="_blank">
-                        <div className="lg:text-sm text-[12px] font-medium mx-2 font-satoshiMed">JOIN WAITLIST</div>
+                        {/* <div className="lg:text-sm text-[12px] font-medium mx-2 font-satoshiMed">JOIN WAITLIST</div> */}
                     </a>
                     <a href="https://dh4ftmjzfzn.typeform.com/to/XuzeNABu" target="_blank">
                         <div className="lg:text-sm text-[12px] whitespace-nowrap font-medium mx-2 font-satoshi bg-[#0A47F2] text-white px-[25px] py-[10px] rounded-full">Sign Up</div>
